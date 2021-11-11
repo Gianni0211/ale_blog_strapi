33841d8597206cfe43f58524ae1bf06749288c75
Gianni Mondello <gianni.mondello@objectmethod.it>
2021-11-11 15:17:54 +0100

    primo commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ba7c13d1ff369193d77910b01219774f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0290a4b2-7461-4e70-bd42-293f83f33a73'
+};
